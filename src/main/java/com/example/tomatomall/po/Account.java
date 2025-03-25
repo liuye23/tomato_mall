114392c969567b8f49612c6c419704dadb4dd524
liuye23 <231250013@smail.nju.edu.cn>
2025-03-25 15:19:08 +0800

    1

--- src/main/java/com/example/tomatomall/po/Account.java
+++ src/main/java/com/example/tomatomall/po/Account.java
@@ -10,4 +10,10 @@ import lombok.Setter;
 public class Account {
     private String username;
     private String password;
+    private String role;
+    private String email;
+    private String phone;
+    private String address;
+    private String realName;
+    private String idCard;
 }
